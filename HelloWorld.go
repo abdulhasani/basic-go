80ed4dffc34af2ea8c5c3293ac6e4cb969a90b29
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-17 23:15:01 +0700

    with import fmt, you can use func Println

---
+++ HelloWorld.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//mencetak secara biasa
+	fmt.Println("Abdul Kadir Hasani")
+	//mencetak double parameter strin dan decimal
+	fmt.Println("1+1 =", 1+1)
+	//kondisi bernilai boolean
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+	fmt.Println()
+	fmt.Print("My Name is")
+	fmt.Print(" Abdul Kadir Hasani")
+}
