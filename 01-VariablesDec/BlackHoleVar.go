6bb7e786a07b4ecdaa4bff94c2f65c61b1b299d6
Hasani <abdul.kadir.hasani@basajans.com>
2017-03-20 19:20:50 +0700

    blackhole variable

---
+++ 01-VariablesDec/BlackHoleVar.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/** keyword _ merubakan variabel yang tidak dapat ditampilka alias tidak bisa dipakai
+	dalam kata lain _ merupakan keranjang sampah
+	*/
+	_ = "Abdul"
+	name, _ := "hasani", "inas"
+	fmt.Println(name)
+}
